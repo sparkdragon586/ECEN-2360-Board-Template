bf43898ce3718d99c74f63e1cd9f82a4ce901f85
Jack <sparkdragon586@gmail.com>
2025-10-03 16:14:44 -0600

    Made helper script safer

--- tools/new-project.sh
+++ tools/new-project.sh
@@ -1,4 +1,11 @@
 #! /bin/sh
+set -e
+
+if [ -z "$PROJECT_ROOT" ]; then
+  echo "Please run helper scripts inside of dev shell."
+  exit 1
+fi
+
 cp -r $PROJECT_ROOT/template-project $PROJECT_ROOT/$1
 cd $PROJECT_ROOT/$1
 pio pkg install
